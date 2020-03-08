4fe933bbf0ce1196878cd15d35fb082f7d06c358
Chapman Shoop <chapman.shoop@gmail.com>
2020-03-08 12:05:50 -0700

    Use the lts-14.27 snapshot and random-fu library.

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,10 @@
 module Main where
 
-import Lib
+import Data.Random
+
+locations = ["Twitter", "Blog", "Forum", "Discord", "Reddit"]
 
 main :: IO ()
-main = someFunc
+main = do
+    location <- sample $ randomElement locations
+    putStrLn location
